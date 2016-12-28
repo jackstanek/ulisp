f94e578f610199982c2868989ef89e4167b33eb1
Jack Stanek <stanekjack@gmail.com>
2016-12-28 17:51:27 -0600

    Fix segfault if EOF given to readline

--- src/repl.c
+++ src/repl.c
@@ -1,12 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <readline/readline.h>
+#include <readline/history.h>
 
+/**
+ * Infinitely runs a read-eval-print-loop.
+ * Currently, "eval" means "print verbatim"
+ */
 void repl(const char* prompt) {
     char* input;
-    do {
+    while (1) {
         input = readline(prompt);
-        puts(input);
-        free(input);
-    } while (input);
+        if (input) {
+            /* Check if line is non-empty */
+            if (*input) {
+                puts(input);
+                add_history(input);
+            }
+
+            free(input);
+        } else {
+            return;
+        }
+    }
 }
